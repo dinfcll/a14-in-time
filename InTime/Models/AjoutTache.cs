c5efb43628cb9e00945e8e8da73ded46a701066e
xxxnemesis4xxx <magnitude9@live.ca>
2014-09-05 15:34:04 -0400

    Variables

--- InTime/Models/AjoutTache.cs
+++ InTime/Models/AjoutTache.cs
@@ -7,5 +7,10 @@ namespace InTime.Models
 {
     public class AjoutTache
     {
+        private string m_strNomTache;
+        private string m_strLieu;
+        private string m_strDescTache;
+        private DateTime m_dtHeure;
+        private DateTime m_dtRappel;
     }
 }
