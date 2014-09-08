b2e1a5c4d97f90568e74c4f16fbc49d85f9643ad
xxxnemesis4xxx <magnitude9@live.ca>
2014-09-08 13:37:31 -0400

    Visual
    Visuel Operationnel

--- InTime/Controllers/AjouterTacheController.cs
+++ InTime/Controllers/AjouterTacheController.cs
@@ -11,9 +11,28 @@ namespace InTime.Controllers
         //
         // GET: /AjouterTache/
 
-        public ActionResult Index()
+        public ActionResult Index(string Min,string Heure)
         {
+            var trancheMin = new List<string>();
+            string[] tempsMin = { "0", "15", "30", "45", "60" };
+            trancheMin.AddRange(tempsMin);
+
+            var trancheHeure = new List<string>();
+            for (int i = 1; i < 25; ++i)
+                trancheHeure.Add(Convert.ToString(i));
+
+
+                ViewBag.trancheMin = new SelectList(trancheMin);
+            ViewBag.trancheHeure = new SelectList(trancheHeure);
+
             return View();
+        }
+
+
+        //Action qui se produit suite \ l'appuie du bouton enregistrer
+        private void InsertionTache(string m_strNomTache,string m_strLieu,string m_strDescTache,DateTime m_dtHeure,DateTime m_dtRappel)
+        {
+
         }
 
     }
--- InTime/Models/AjoutTache.cs
+++ InTime/Models/AjoutTache.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace InTime.Models
 {
     public class AjoutTache
     {
-        private string m_strNomTache;
-        private string m_strLieu;
-        private string m_strDescTache;
-        private DateTime m_dtHeure;
-        private DateTime m_dtRappel;
+        [Required(ErrorMessage = "Obligatoire")]
+        public string m_strNomTache {get; set;}
+
+        [Required(ErrorMessage = "Obligatoire")]
+        public string m_strLieu { get; set; }
+
+        [Required(ErrorMessage = "Obligatoire")]
+        public string m_jour { get; set; }
+        [Required(ErrorMessage = "Obligatoire")]
+        public string m_mois { get; set; }
+        [Required(ErrorMessage = "Obligatoire")]
+        public string m_annee { get; set; }
+
+
+        public string m_strDescTache { get; set; }
+        public string m_debHeure { get; set; }
+        public string m_debMin { get; set; }
+        public string m_finHeure { get; set; }
+        public string m_finMin { get; set; }
+        public string m_rappelHeure { get; set; }
+        public string m_rappelMin { get; set; }
+        public DateTime m_dtDebut { get; set; }
+        public DateTime m_dtFin { get; set; }
+        public DateTime m_dtRappel { get; set; }
     }
 }
