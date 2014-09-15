6b42f6f2d917e98be1096894bc9608639633e284
xxxnemesis4xxx <magnitude9@live.ca>
2014-09-15 13:03:39 -0400

    Initial

---
+++ InTime/Controllers/ConsulterTacheController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InTime.Models;
+
+namespace InTime.Controllers
+{
+    public class ConsulterTacheController : Controller
+    {
+        public ActionResult Index()
+        {
+            var tache1 = new AjoutTache()
+            {
+                m_strNomTache = "Test",
+                m_strDescTache = "C'est un test",
+                m_annee = "2014",
+                m_mois = "Janvier",
+                m_jour = "15",
+                m_debHeure = "9",
+                m_debMin = "0",
+                m_finHeure = "17",
+                m_finMin = "30",
+                m_rappelHeure = "",
+                m_rappelMin = "",
+                m_strLieu = "Bureau"
+            };
+
+            //TODO :
+            //Aller chercher l'information dans la base de donnee
+
+            return View(tache1);
+        }
+
+    }
+}
