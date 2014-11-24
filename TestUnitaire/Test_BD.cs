5b60fbbae5cf0cd9580fdcdb38db24c2f2c06bff
Max1991 <max.breton@hotmail.fr>
2014-11-24 12:41:19 -0500

    Test connexion string

---
+++ TestUnitaire/Test_BD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InTime.Filters;
+using InTime.Models;
+using System.Data.SqlClient;
+
+namespace TestUnitaire
+{
+    [TestClass]
+    public class Test_BD
+    {
+        [TestMethod]
+        public bool Connexionstring()
+        {
+            string ConString = @"Data Source=EQUIPE-02\SQLEXPRESS;Initial Catalog=InTime;Integrated Security=True";
+            return true;
+        }
+    }
+}
---
+++ UnitTestConnexion/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InTime.Filters;
+using InTime.Models;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestConnexion
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConnexionString()
+        {
+            string Constring = @"Data Source=EQUIPE-02\SQLEXPRESS;Initial Catalog=InTime;Integrated Security=True";
+            SqlConnection connect = new SqlConnection(Constring);
+            Assert.IsNotNull(connect);
+        }
+    }
+}
