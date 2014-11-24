34dbee0495b6880f8c383986cb042439281578d4
Max1991 <max.breton@hotmail.fr>
2014-11-24 14:09:52 -0500

    Test1 Interface+Real & Dummy Class

--- InTime/ConnexionUtilisateur.cs
+++ InTime/ConnexionUtilisateur.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InTime.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace InTime
 {
     interface ConnexionUtilisateur
     {
+        void CreerUsager(RegisterModel model)
+        {
+
+        }
+
+        void LoginUsager(RegisterModel model)
+        {
+
+        }
     }
 }
